601bc3fc99a24a04de7be01734067f18e870ec88
oracle <oracle@localhost>
2019-10-15 13:14:41 +0200

    Ejercicio 13: Ejercicio similar al Eje 12 pero en vez de serializar objectos "normales" se serializan arrys de objetos 1º) Crear un FileOutputSTream para asi poder escribir mediante un archivo ObjectOutput. 2º)Crear una subclase productos serializable. 3º) Creamos tres arrays de datos: dos string y uno double
    ????Necesita correcion

--- src/serializacion2/Serializacion2.java
+++ src/serializacion2/Serializacion2.java
@@ -1,16 +1,18 @@
 
 package serializacion2;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 
 public class Serializacion2 {
     
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
       String []codigo={"p1","p2","p3"};
       String []descricion={"botellas","chicles","pan"};
       double []precio={1.0,2.0,3.0};
@@ -25,23 +27,35 @@ public class Serializacion2 {
         escribir1.close();
         escribir.close();
         
+        
+          FileInputStream leer = new FileInputStream("/home/oracle/Desktop/Eje12/productos.txt");
+              ObjectInputStream leer1 = new ObjectInputStream(leer);
+              
+              Object datos = 0;
+              
+              while (datos != null){
+                  if(datos == null){
+                      System.out.println("Termino de datos");
+                      
+                  }else{
+                      datos = leer1.readObject();
+                      System.out.println(datos);
+                      
+                  }
+              }
+              leer1.close();
+          }
     }
     }
     
-}
+
 
 class Productos implements Serializable{
     String codigo;
     String descricion;
     double precio;
 
-    public Productos() {
-        codigo ="";
-        descricion = "";
-        precio = 0;
-        
-        
-    }
+    
 
     public Productos(String codigo, String descricion, double precio) {
         this.codigo = codigo;
@@ -78,6 +92,9 @@ class Productos implements Serializable{
         return "Productos{" + "codigo=" + codigo + ", descricion=" + descricion + ", precio=" + precio + '}';
     }
     
+    //Necesita revision
+    //Tiene algun gfallo al ejecutar
+    //mirar que pasa
     
     
     
